99cf789d660aa90f7b48b02ac0dd76779fd0e382
grebencukkirill <grebencukkirill@gmail.com>
2022-10-29 17:57:51 +0300

    PZ

--- PZ-5/PZ-5-1.py
+++ PZ-5/PZ-5-1.py
@@ -1,10 +1,10 @@
-
+#Составить функцию, которая выполнит сложение числового ряда
 
 def count(a):
     sum = 0
     for i in range(len(a)):
         sum += a[i]
     return sum
-nums = (list(map(int, (input()).split())))
+nums = (list(map(int, (input('Введите ряд чисел: ')).split())))
 
 print('сумма: ', count(nums))
