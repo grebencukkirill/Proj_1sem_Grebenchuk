389faf902b3efb272b94bde21af4fd3d51542fd7
Abyka12 <antondvachevsky@yandex.ru>
2022-11-22 10:47:57 +0300

    PZ

--- PZ-6/PZ-6-2.py
+++ PZ-6/PZ-6-2.py
@@ -1,14 +1,32 @@
-# Дан список размера N. Найти номер его первого локального минимума (локальный минимум - это элемент, который меньше любого из своих соседей).
+# Дан список размера N.
+# Найти номер его первого локального минимума (локальный минимум - это элемент, который меньше любого из своих соседей).
 
-nums = (list(map(int, (input('Введите ряд чисел: ')).split())))
+import random
+a = input('Введите число: ') #Ввод числа
+
+while type(a) != int: #Обработка исключений
+    try:
+        a = int(a)
+    except ValueError:
+        print('Неправильно ввели!')
+        a = input('Введите число: ')
+nums = []
+for i in range(a):
+    nums.append(random.randint(0, 10))
+print(*(nums))
 i = 0
 for num in nums:
-    if i == len(nums)-1:
+    if i == len(nums)-1 and len(nums) != 1:
+        if (num < nums[i - 1]) and (num < nums[0]):
+            print('Первый локальный минимум: ', num)
+            break
         print('Локальный минимум отсутствует')
-        print(i)
+        break
+    else:
+        print('Первый локальный минимум: ', nums[0])
         break
     if (num < nums[i-1]) and (num < nums[i+1]):
-        print('Локальный минимум: ', num)
+        print('Первый локальный минимум: ', num)
         break
     i += 1
 else:
