22551ddac27c7f2d45b9eadbbb57c4bf1266ea77
Danil <05danilca@mail.ru>
2023-01-21 15:18:16 +0300

    PZ

--- PZ-11/PZ-11-1.py
+++ PZ-11/PZ-11-1.py
@@ -1,4 +1,4 @@
-import random
+import numpy as np
 
 l = '56 -98 48 -21 21 -47'
 
@@ -6,15 +6,17 @@ f1 = open('input_data.txt', 'w')
 f1.writelines(l)
 f1.close()
 
-
-f2 = open('input_data.txt')
-k = f2.read()
-k.split()
+k = list(map(int, (((open('input_data.txt')).read()).split())))
 sq = []
 sum = 0
 for i in range(len(k)):
-    if k[i] == 0:
+    if k[i] % 2 == 0:
         sq.append(k[i]**2)
         sum += k[i]**2
-print(k)
-print('Исходные данные: ', l,'Количество элементов: ', len(k),'Минимальный элемент', min(k),'Квадраты четных элементов', *sq ,'Сумма квадратов четных элементов', sum ,'Среднее арифметическое суммы квадратов четных элементов')
+f3 = open('output_data.txt', 'w')
+print('Исходные данные:', str(l),
+      '\nКоличество элементов:', str(len(k)),
+      '\nМинимальный элемент:', str(min(k)),
+      '\nКвадраты четных элементов:', *list(map(str, (sq))),
+      '\nСумма квадратов четных элементов:', str(sum),
+      '\nСреднее арифметическое суммы квадратов четных элементов:', str(np.mean(sq)), file=f3)
