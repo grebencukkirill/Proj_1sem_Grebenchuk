6e35492fa9f7f3a3a669a2c1584000e13aed1b58
Danil <05danilca@mail.ru>
2023-01-21 16:26:51 +0300

    PZ

--- PZ-11/PZ-11-2.py
+++ PZ-11/PZ-11-2.py
@@ -1,6 +1,24 @@
-stih = 'Мы долго молча отступали, \nДосадно было, боя ждали, \nВорчали старики: \n«Что ж мы? на зимние квартиры? \nНе смеют, что ли, командиры \nЧужие изорвать мундиры \nО русские штыки?»'
-f1 = open('stih.txt', 'w')
-f1.write(stih)
-znaki = ',.!?;:'
-sum, j = 0, 0
-for i in open('stih.txt'):
+# Из предложенного текстового файла (text18-3.txt) вывести на экран его
+# содержимое, количество знаков пунктуации в первых четырех строках.
+# Сформировать новый файл, в который поместить текст в стихотворной форме
+# предварительно заменив символы третей строки их числовыми кодами.
+f1 = open('text18-3.txt')
+print(f1.read())
+text = open('text18-3.txt').readlines()
+f1.close()
+sum = 0
+digits = ''
+stih = ''
+for i in range(4):
+    sum += text[i].count('.') + text[i].count(',') + text[i].count('!') + text[i].count('?') + text[i].count(':') + text[i].count(';')
+for i in range(len(text[2])-2):
+    digits += str(ord(text[2][i]))
+    digits += ' '
+f2 = open('stih.txt', 'w')
+for i in range(len(text)):
+    if i == 2:
+        f2.write(digits + '\n')
+        continue
+    f2.write(text[i])
+f2.close()
+print('Количество знаков пунктуации в первых четырех строках: ', sum)
