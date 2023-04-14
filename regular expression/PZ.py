00dae0d1579b1af8ff4a91043afb635e4d02cd9d
grebencukkirill <82510937+grebencukkirill@users.noreply.github.com>
2023-04-14 12:41:50 +0300

    PZ

---
+++ regular expression/PZ.py
@@ -0,0 +1,30 @@
+import re
+
+with open('text.txt') as file1:
+    text = file1.read()
+p1 = re.compile(r'\b\d+\b')
+p2 = re.compile(r'\b[A-Я]+\b')
+p3 = re.compile(r'\b\w*[А-Яа-я]\d\w*\b')
+p4 = re.compile(r'\b[А-ЯA-Z]\w*\b')
+p5 = re.compile(r'\b[аеёиоуыэюяАЕЁИОУЫЭЮЯ]\w*\b')
+p6 = re.compile(r'(?<!\b)\d+(?!\b)')
+p7 = re.compile(r'^.*\*.*$')
+p8 = re.compile(r'^.*\(.*\).*$ ')
+p9 = re.compile(r'')
+p10 = re.compile(r'')
+p11 = re.compile(r'^\s*$')
+p12 = re.compile(r'<[^>]+>')
+
+print(re.findall(p1, text))
+print(re.findall(p2, text))
+print(re.findall(p3, text))
+print(re.findall(p4, text))
+print(re.findall(p5, text))
+print(re.findall(p6, text))
+print(re.findall(p7, text))
+print(re.findall(p8, text))
+print(re.findall(p9, text))
+print(re.findall(p10, text))
+print(re.findall(p11, text))
+print(re.findall(p12, text))
+
