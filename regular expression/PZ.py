d46febae178aafa448a0d710da030a0c975d15d6
grebencukkirill <82510937+grebencukkirill@users.noreply.github.com>
2023-04-15 12:31:02 +0300

    PZ

--- regular expression/PZ.py
+++ regular expression/PZ.py
@@ -1,17 +1,20 @@
 import re
 
 with open('text.txt') as file1:
-    text = file1.read()
-p1 = re.compile(r'\b\d+\b')
-p2 = re.compile(r'\b[A-Я]+\b')
-p3 = re.compile(r'\b\w*[А-Яа-я]\d\w*\b')
+    lines = file1.readlines()
+text = ''
+for line in lines:
+    text += line + '\n'
+p1 = re.compile(r'\b\d+\b|\b\w*\d+\w*\b')
+p2 = re.compile(r'\b[A-ЯA-Z]+\b|\b[A-ЯA-Z]+[а-яa-z]*[A-ЯA-Z]+\b')
+p3 = re.compile(r'\b\w*[а-яА-Я]+\d\w*\b')
 p4 = re.compile(r'\b[А-ЯA-Z]\w*\b')
 p5 = re.compile(r'\b[аеёиоуыэюяАЕЁИОУЫЭЮЯ]\w*\b')
 p6 = re.compile(r'(?<!\b)\d+(?!\b)')
 p7 = re.compile(r'^.*\*.*$')
-p8 = re.compile(r'^.*\(.*\).*$ ')
-p9 = re.compile(r'')
-p10 = re.compile(r'')
+p8 = re.compile(r'^.*\(.*$.*\)|^.*\(.*\).*$')
+p9 = re.compile(r'<(\w+)[^>]*>(.*?)<\/\1>')
+p10 = re.compile(r'>([^<]+)<')
 p11 = re.compile(r'^\s*$')
 p12 = re.compile(r'<[^>]+>')
 
